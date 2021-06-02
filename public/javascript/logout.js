ff866e36a68b9d713011b1b82c39a3fc38c172d6
andrp <a.poklar@yahoo.com>
2021-06-01 21:01:07 -0500

    added handlebars

---
+++ public/javascript/logout.js
@@ -0,0 +1,26 @@
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+});
+
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+}
+  
+document.querySelector('#logout').addEventListener('click', logout);
+  
